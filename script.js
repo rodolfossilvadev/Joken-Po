574c860eeabde3d802cb0394715195e2961c1ed2
rodolfossilvadev <rodolfodev30@gmail.com>
2024-09-13 23:15:12 -0300

    Primeiro commit jokenpo

---
+++ script.js
@@ -0,0 +1,55 @@
+const result = document.querySelector(".result")
+const choiceMachine = document.querySelector(".choice-machine")
+const myScore = document.querySelector("#human-score")
+const machineScore = document.querySelector("#machine-score")
+
+let humanScoreNumber = 0
+let machineScoreNumber = 0
+
+
+const playHuman = (humamChoise) => {
+    playTheGame(humamChoise, playMacine())
+    winTheGame()
+
+}
+const playMacine = () => {
+    const choices = ['rock', 'paper', 'scissors']
+    const randomNumber = Math.floor(Math.random() * 3)
+
+    choiceMachine.innerHTML = `IA escolheu: ${randomNumber}` 
+
+    return choices[randomNumber]
+}
+const playTheGame = (human, machine) => {
+    console.log(' Humano: ' + human + ' Máquina: ' + machine)
+
+    if (human === machine) {
+        result.innerHTML = "Empate!"
+    } else if ((human === 'paper' && machine === 'rock') ||
+        (human === 'rock' && machine === 'scissors') ||
+        (human === 'scissors' && machine === 'paper')) {
+            humanScoreNumber++
+            myScore.innerHTML = humanScoreNumber
+            result.innerHTML = "&#x2705 Você GANHOU!  &#x2705"
+    } else {
+        machineScoreNumber++
+        machineScore.innerHTML = machineScoreNumber 
+        result.innerHTML = "&#x274C Você perdeu! &#x274C"
+    }
+
+}
+
+const winTheGame = (human, machine) =>{
+
+    if(humanScoreNumber === 5){
+       
+         result.innerHTML = "CAMPEÃOOO! &#x1F3C6 &#x1F3C6 &#x1F3C6"
+         myScore.innerHTML = 0
+         machineScore.innerHTML = 0
+    } else if (machineScoreNumber === 5){
+        result.innerHTML = "IA Venceu"
+        myScore.innerHTML = 0
+        machineScore.innerHTML = 0
+    }
+
+}
